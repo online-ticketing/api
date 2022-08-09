46f40bc8de3332b9012e21079ec40dd7fb37d4e9
fboakye008 <107209416+fboakye008@users.noreply.github.com>
2022-08-09 14:14:27 -0400

    Adding api key

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,30 @@
 'use strict';
 
-module.exports = function(Appuser) {
+module.exports = function(User) {
+    //?access_token=adfdafda
+  User.beforeRemote("**", async function (ctx, ununsed) {
+    let api_key = ctx.req.headers.access_token || ctx.req.query.access_token || ctx.req.body.access_token;
+    //create if no api key allow
+    console.log("api_key",api_key);
+    const methodName = ctx.method.name;
+    const list = ["patchorcreate","find","replaceorcreate","deleteById"]
+    const allow = ["create"];
+    const user_admin= [
+      "patchattributes",
+      'findbyid',
+      'exists',
+      '__delete__bookings',
+      'replacebyid',
+      '__get__bookings',
+      '__create__bookings',
+      '__findById__bookings',
+      '__updateById__bookings',
+      '__destroyById__bookings',
+      '__get__roles',
+      '__create__roles'
+    ];
 
+    console.log("methodName",methodName);
+    return;
+  });
 };
