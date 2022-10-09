6021a8f64d956ac0f25447daafd008823d72a108
Frank Boakye <fboakye008@st.ug.edu.gh>
2022-10-09 06:06:37 -0400

    Adding connection string

--- common/models/user.js
+++ common/models/user.js
@@ -4,8 +4,6 @@ module.exports = function(User) {
     //?access_token=adfdafda
   User.beforeRemote("**", async function (ctx, ununsed) {
     let api_key = ctx.req.headers.access_token || ctx.req.query.access_token || ctx.req.body.access_token;
-    //create if no api key allow
-    console.log("api_key",api_key);
     const methodName = ctx.method.name;
     const list = ["patchorcreate","find","replaceorcreate","deleteById"]
     const allow = ["create"];
@@ -24,7 +22,6 @@ module.exports = function(User) {
       '__create__roles'
     ];
 
-    console.log("methodName",methodName);
     return;
   });
 };
