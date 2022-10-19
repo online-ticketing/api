a937673d3833b59464c042f5720014558d0e1466
jasiedu <jasiedu@broadinstitute.org>
2022-10-18 22:05:33 -0400

    Adding a script to populate database

--- sql/script_01.sql
+++ sql/script_01.sql
@@ -14,7 +14,6 @@ CREATE TABLE IF NOT EXISTS `online_ticketing`.`user` (
   `full_name` varchar(50) NOT NULL,
   `contact_number` varchar(10) NOT NULL,
   `email` varchar(30) DEFAULT NULL,
-  `username` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
   `api_key` VARCHAR(45) NOT NULL,
   `account_status` int NOT NULL DEFAULT '0',
@@ -22,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `online_ticketing`.`user` (
   `last_modified` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `contact_number_UNIQUE` (`contact_number`),
-  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `api_key_UNIQUE` (`api_key`)
 );
 
@@ -175,7 +174,7 @@ CREATE TABLE IF NOT EXISTS `online_ticketing`.`ticket` (
   `id` int NOT NULL AUTO_INCREMENT,
   `seat_number` int NOT NULL,
    `user_id` int NOT NULL,
-  `serial_number` varchar(30) NOT NULL,
+  `serial_number` varchar(150) NOT NULL,
   `booking_id` int NOT NULL,
   `date_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `last_modified` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -187,99 +186,53 @@ CREATE TABLE IF NOT EXISTS `online_ticketing`.`ticket` (
   CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`)
 );
 
+CREATE TABLE `online_ticketing`.`recoveries`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `expiry` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `otp` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_r_expiry` (`expiry`)
+);
+-- insert users
+INSERT INTO `online_ticketing`.`user` (`full_name`,`contact_number`,`email`,`password`,`account_status`,`api_key`)
+VALUES("Kwabena Asiedu","6174819043","jacob.asiedu@gmail.com","$2a$10$ZldWMzJiLeoUlyyFOxiiPeVmROBYsY3V7f/NXw.rIywfZDbrlP8eS",1,'xxxxxxxxx1'),
+("Frank Boakye","0245666208","boakyef213@gmail.com","$2a$10$ZldWMzJiLeoUlyyFOxiiPeVmROBYsY3V7f/NXw.rIywfZDbrlP8eS",1,'xxxxxxxxx2');
 
-INSERT INTO `online_ticketing`.`user` (`full_name`,`contact_number`,`email`,`username`,`password`,`account_status`,`api_key`)
-VALUES("Kwabena Asiedu","6174819043","jacob.asiedu@gmail.com","jasiedu","$2a$10$ZldWMzJiLeoUlyyFOxiiPeVmROBYsY3V7f/NXw.rIywfZDbrlP8eS",1,'xxxxxxxxx1'),
-("Frank Boakye","0245666208","boakyef213@gmail.com","boakyef","$2a$10$ZldWMzJiLeoUlyyFOxiiPeVmROBYsY3V7f/NXw.rIywfZDbrlP8eS",1,'xxxxxxxxx2'),
-("Adu Acheampong","0591984773","aacheampong@gmail.com","aacheampong","$2a$10$ZldWMzJiLeoUlyyFOxiiPeVmROBYsY3V7f/NXw.rIywfZDbrlP8eS",1,'xxxxxxxxx3');
-
+-- Insert Drivers, Managers and Admin
 INSERT INTO `online_ticketing`.`role` (`name`,`created_by_id`,`description`)
 VALUES("MANAGER",1,"The Manager Role"),("BOOK_MAN",1,"The Book man Role"),("DRIVER",1,"The Driver Role"),("ADMIN",1,"The System Administrative Role"),("CONDUCTOR",1,"The Conductor Role"),("PASSENGER",1,"The passenger Role");
 
 INSERT INTO `online_ticketing`.`user_role` (`user_id`,`role_id`)
 select distinct u.id as user_id, r.id as role_id
 from online_ticketing.user u cross join online_ticketing.role r
-where u.username='jasiedu' and r.name='MANAGER';
+where u.email='jacob.asiedu@gmail.com' and r.name='MANAGER';
 
 INSERT INTO `online_ticketing`.`user_role` (`user_id`,`role_id`)
 select distinct u.id as user_id, r.id as role_id
 from online_ticketing.user u cross join online_ticketing.role r
-where u.username='jasiedu' and r.name='ADMIN';
+where u.email='jacob.asiedu@gmail.com' and r.name='ADMIN';
 
 INSERT INTO `online_ticketing`.`user_role` (`user_id`,`role_id`)
 select distinct u.id as user_id, r.id as role_id
 from online_ticketing.user u cross join online_ticketing.role r
-where u.username='jasiedu' and r.name='PASSENGER';
+where u.email='jacob.asiedu@gmail.com' and r.name='PASSENGER';
 
 INSERT INTO `online_ticketing`.`user_role` (`user_id`,`role_id`)
 select distinct u.id as user_id, r.id as role_id
 from online_ticketing.user u cross join online_ticketing.role r
-where u.username='boakyef' and r.name='DRIVER';
+where u.email='boakyef213@gmail.com' and r.name='DRIVER';
 
 INSERT INTO `online_ticketing`.`user_role` (`user_id`,`role_id`)
 select distinct u.id as user_id, r.id as role_id
 from online_ticketing.user u cross join online_ticketing.role r
-where u.username='boakyef' and r.name='PASSENGER';
+where u.email='boakyef213@gmail.com' and r.name='PASSENGER';
 
 
 INSERT INTO `online_ticketing`.`user_role` (`user_id`,`role_id`)
 select distinct u.id as user_id, r.id as role_id
 from online_ticketing.user u cross join online_ticketing.role r
-where u.username='aacheampong' and r.name='CONDUCTOR';
-
-INSERT INTO `online_ticketing`.`user_role` (`user_id`,`role_id`)
-select distinct u.id as user_id, r.id as role_id
-from online_ticketing.user u cross join online_ticketing.role r
-where u.username='aacheampong' and r.name='PASSENGER';
-
-INSERT INTO `online_ticketing`.`bus`(`bus_number`,`plate_number`,`capacity`,`gps_location`,`created_by_id`,`status`)
-VALUES(1,"GHA-A-23456",120,POINT(50.0755381, 14.4378005),1,1),
-(12,"GHA-B-23456",80,POINT(50.0755381, 14.4378005),1,1);
-
-INSERT INTO `online_ticketing`.`route`(`name`,`created_by_id`,`fare`)
-VALUES("ACCRA-KUMASI",1,45.35),("KUMASI-ACCRA",1,45.35);
-
-
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Kwame Nkrumah Circle, Dr. Busia Hwy, Accra, Ghana',POINT(-0.210089,5.545935),'GP1',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Achimota Old Station Bus Stop, Accra, Ghana',POINT(-0.229495,5.615458),'GP2',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('China Mall Amasaman, PM8V+G82, Amasaman, Ghana',POINT(-0.124403,5.639194),'GP3',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('MEDIE, QM6G+RJW, Medie, Ghana',POINT(-0.32598,5.761949),'GP4',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Teye Lawer Ent., RJ9W+MCR, Nsawam Road, Nsawam, Ghana',POINT(-0.35026,5.80893),'GP5',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('MCANIM SERVICE STATION, Nsawam - Suhum Rd, Teacher Mante, Ghana',POINT(-0.982714,7.85125),'GP6',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Asuboi Health centre,Asuboe, Ghana',POINT(-76.746438,38.948739),'GP7',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Sankofa filling Station,Amanase, Ghana',POINT(0.002234,5.786453),'GP8',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Asafo Market, Nhyiaeso, Accra, Ghana',POINT(-1.615856,6.688726),'GP9',1);
-
-
-
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Asafo Market, Nhyiaeso, Accra, Ghana',POINT(-1.615856,6.688726),'GP10',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Hotel Georgia, Ahodwo Round About, Southern By-Pass, Kumasi, Ghana',POINT(-1.62443,6.68848),'GP11',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Kumasi City Mall, Lake Rd, Kumasi, Ghana',POINT(-1.62443,6.68848),'GP12',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Tech Junction overpass bridge, MCPG+PFG, Ejisu Road, Kumasi, Ghana',POINT(-1.62443,6.68848),'GP13',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES("Kindle Organic, Ejisu Kwamu main road, beside Kwamu Chief's Palace Digital Address: AE-0197-3789, Kumasi, Ghana",POINT(-1.62443,6.68848),'GP14',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Anita Hotel, Kumasi, Konongo - Ejisu Rd, Ghana',POINT(-0.982714,7.85125),'GP15',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Pacific filling station, MJ7P+W9, Kubease, Ghana',POINT(0.002234,5.786453),'GP16',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Konongo Market, Konongo, Ghana',POINT(-1.211731,6.623536),'GP17',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('JULAN HOTEL, HVRM+PCW, Atwedie Road, Juaso, Ghana',POINT(-1.13333,6.6),'GP18',1);
-INSERT INTO online_ticketing.bus_stop(name,gps_location,ghana_post_address, created_by_id) VALUES('Kwame Nkrumah Circle, Dr. Busia Hwy, Accra, Ghana',POINT(-0.210089,5.545935),'GP19',1);
-
-
-
-
---
--- INSERT INTO online_ticketing.route_bus_stop
-INSERT INTO `online_ticketing`.`route_bus_stop` (`route_id`,`bus_stop_id`,`seq_order`)
-select distinct r.id as route_id, bs.id as bus_stop_id,bs.id
-from online_ticketing.route r cross join online_ticketing.bus_stop bs
-where r.name='ACCRA-KUMASI' and bs.ghana_post_address in ('GP1','GP2','GP3','GP4','GP5','GP6','GP7','GP8','GP9');
-
-
-
-INSERT INTO `online_ticketing`.`route_bus_stop` (`route_id`,`bus_stop_id`,`seq_order`)
-select distinct r.id as route_id, bs.id as bus_stop_id,bs.id
-from online_ticketing.route r cross join online_ticketing.bus_stop bs
-where r.name='KUMASI-ACCRA' and bs.ghana_post_address in ('GP10','GP11','GP12','GP13','GP14','GP15','GP16','GP17','GP18','GP19');
-
+where u.email='jacob.asiedu@gmail.com' and r.name='DRIVER';
 
 CREATE OR REPLACE view online_ticketing.v_bus_route as
 SELECT
@@ -342,16 +295,8 @@ left join online_ticketing.route r on r.id=bsc.route_id
 left join  online_ticketing.bus bus on bus.id=bsc.bus_id
 left join online_ticketing.user u on u.id=t.user_id
 where b.status=1;
-DROP TABLE IF EXISTS `online_ticketing`.`recoveries`;
-CREATE TABLE `online_ticketing`.`recoveries`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(45) NOT NULL,
-  `expiry` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `otp` varchar(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_r_expiry` (`expiry`)
-);
+
 
 create or replace view online_ticketing.v_recoveries as
-select r.* from recoveries r where expiry > date_sub(now(), interval 10 MINUTE);
+select r.* from online_ticketing.recoveries r where expiry > date_sub(now(), interval 10 MINUTE);
 
